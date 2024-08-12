d3f26ebed34a4a718ff65b91bffa7c10013c7b0e
Romain Vincent <romainfvincent@gmail.com>
2024-08-13 00:37:38 +0200

    add sample with known stats

---
+++ samples/known.go
@@ -0,0 +1,17 @@
+// This file contains:
+  
+// - 6 lines of comment
+// - 6 lines of code
+// - 6 empty lines
+
+package main
+
+func main() {
+
+	if true { /* a multi-line comment with
+
+   an empty line inside, that shares space with a line of code */
+		println(42)
+	}
+
+}
